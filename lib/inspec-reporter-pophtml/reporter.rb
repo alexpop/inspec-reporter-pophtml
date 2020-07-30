ad15a1d01be082dbf0cdb48a34e790161eb2fd20
Alex Pop <al3xpop@gmail.com>
2020-07-30 11:22:06 +0200

    Add profile level sums and statuses and html improvements
    Signed-off-by: Alex Pop <al3xpop@gmail.com>

--- lib/inspec-reporter-pophtml/reporter.rb
+++ lib/inspec-reporter-pophtml/reporter.rb
@@ -14,8 +14,15 @@ module InspecPlugins::PopHtmlReporter
       js_path = cfg[:alternate_js_file] || (template_path + "/default.js")
       css_path = cfg[:alternate_css_file] || (template_path + "/default.css")
 
-      report_uuid = SecureRandom.uuid
-      control_sums = calculate_controls_sums()
+      report_extras = {
+        'uuid' => SecureRandom.uuid,
+        'profiles' => {},
+        'sums' => {},
+        'status' => ''
+      }
+
+      calculate_controls_sums(report_extras)
+      calculate_statuses(report_extras)
 
       template = ERB.new(File.read(template_path + "/body.html.erb"))
       output(template.result(binding))
@@ -25,24 +32,74 @@ module InspecPlugins::PopHtmlReporter
       "~> 0.0"
     end
 
-    # Sums up the controls based on their status
-    def calculate_controls_sums()
-      sums = {
+    # Calculates control sums based on their status
+    def calculate_controls_sums(extras)
+      sums_report = {
         'failed' => 0,
         'passed' => 0,
         'skipped' => 0,
-        'waived' => 0
+        'waived' => 0,
+        'total' => 0
       }
       if run_data['profiles'] != nil && run_data['profiles'].length > 0
         run_data['profiles'].each do |profile|
+          sums_profile = {
+            'failed' => 0,
+            'passed' => 0,
+            'skipped' => 0,
+            'waived' => 0,
+            'total' => 0
+          }
           if profile['controls'] != nil && profile['controls'].length > 0
             profile['controls'].each do |control|
-              sums[status(control)] += 1
+              sums_profile[status(control)] += 1
+            end
+            sums_profile['total'] = sums_profile['failed'] + sums_profile['passed'] + sums_profile['skipped'] + sums_profile['waived']
+            extras['profiles'][profile.sha256] = {}
+            extras['profiles'][profile.sha256]['sums'] = sums_profile
+
+            sums_report['failed'] += sums_profile['failed']
+            sums_report['passed'] += sums_profile['passed']
+            sums_report['skipped'] += sums_profile['skipped']
+            sums_report['waived'] += sums_profile['waived']
+          end
+        end
+      end
+      sums_report['total'] = sums_report['failed'] + sums_report['passed'] + sums_report['skipped'] + sums_report['waived']
+      extras['sums'] = sums_report
+    end
+
+
+    # Calculates report and profile statuses for reporting
+    def calculate_statuses(extras)
+      # Profile level 'status' & 'status_message' are new to inspec (v4.22.0+)
+      # and are used to flag incompatibilities (target) or issues with the profiles
+      skipped_profiles = 0
+      failed_profiles = 0
+      if run_data['profiles'] != nil && run_data['profiles'].length > 0
+        run_data['profiles'].each do |profile|
+          if profile['status'] == 'failed'
+            failed_profiles += 1
+          elsif profile['status'] == 'skipped'
+            skipped_profiles += 1
+          else
+            if profile['status'].to_s == '' || profile['status'] == 'loaded'
+              extras['profiles'][profile.sha256]['status'] = status_from_sums(extras['profiles'][profile.sha256]['sums'])
+            else
+              extras['profiles'][profile.sha256]['status'] = profile['status']
             end
           end
         end
       end
-      return sums
+
+      if failed_profiles > 0
+        extras['status'] = 'failed'
+      elsif skipped_profiles > 0 && skipped_profiles == run_data['profiles'].length
+        extras['status'] = 'skipped'
+      else
+        # Derive the status of a report from the controls
+        extras['status'] = status_from_sums(extras['sums'])
+      end
     end
 
     # Returns the status of a control
@@ -71,6 +128,18 @@ module InspecPlugins::PopHtmlReporter
       end
       return status
     end
-    
+
+    # Returns the status from the results of a control
+    def status_from_sums(sums)
+      status = 'passed'
+      if sums['failed'] > 0
+        status = 'failed'
+      elsif sums['total'] == sums['skipped'] && sums['skipped'] > 0
+        status = 'skipped'
+      elsif sums['total'] == sums['waived'] && sums['waived'] > 0
+        status = 'waived'
+      end
+      return status
+    end
   end
 end
